0691c419a93187891ba22f543349e93b66b3c19b
Andy Hasselwander <ahasselwander@gmail.com>
2018-03-09 07:26:10 -0500

    Added libraries to ui.R

--- acidRainApp/server.R
+++ acidRainApp/server.R
@@ -1,22 +1,32 @@
 library(shiny)
-shinyServer(function(input, output, session) {
+library(leaflet)
+library(ggplot2)
+library(RColorBrewer)
+library(stringr)
 
-    coordinates <- read.csv("LTM_Sample_Coordinates.csv")
-    coordinates <- coordinates[complete.cases(coordinates),]
-    colnames(coordinates) <- c("lat", "lng", "popup", "ALSC")
-    lakedata <- read.csv("LakeData.csv")
-    colnames(lakedata)[1] <- "confirmedDate"
-    require(stringr)
-    lakedata$ALSC <- str_sub(lakedata$ALSC,2, str_length(lakedata$ALSC))
-    lakedata$confirmedDate <- as.Date(lakedata$confirmedDate, "%Y.%m.%d")
-    dateValues <- unique(lakedata$confirmedDate)
-    measurementValues <- colnames(lakedata)[8:ncol(lakedata)]
-    coordinates_and_data <- merge(coordinates, lakedata, by = "ALSC")
-    require(data.table)
-    coord_data_dt <- data.table(coordinates_and_data)
+
+coordinates <- read.csv("LTM_Sample_Coordinates.csv")
+coordinates <- coordinates[complete.cases(coordinates),]
+colnames(coordinates) <- c("lat", "lng", "popup", "ALSC")
+lakedata <- read.csv("LakeData.csv")
+colnames(lakedata)[1] <- "confirmedDate"
+lakedata$ALSC <- str_sub(lakedata$ALSC,2, str_length(lakedata$ALSC))
+lakedata$confirmedDate <- as.Date(lakedata$confirmedDate, "%Y.%m.%d")
+dateValues <- base::unique(lakedata$confirmedDate)
+measurementValues <- colnames(lakedata)[8:ncol(lakedata)]
+coordinates_and_data <- merge(coordinates, lakedata, by = "ALSC")
+library(data.table)
+coord_data_dt <- data.table(coordinates_and_data)
+
+shinyServer(function(input, output) {
+
+  
+
   
   output$map1 <- renderLeaflet({
    
+    
+    
     lakeDataSelected <- coordinates_and_data[coordinates_and_data$confirmedDate == input$dateInput,]
     require(RColorBrewer)
     colors <- rev(brewer.pal(9, 'YlOrRd'))
@@ -47,10 +57,9 @@ shinyServer(function(input, output, session) {
     
     
     tsDT <- data.frame(coord_data_dt[, mean(eval(parse(text = input$variableInput))), by = confirmedDate])
-    require(ggplot2)
     ggplot(tsDT, aes(confirmedDate, V1)) + geom_line() +
       ylab("Average of All Measurements") + 
-      geom_smooth(method='lm')
+      geom_smooth(method='lm', na.rm=TRUE)
     
   })
   
--- acidRainApp/ui.R
+++ acidRainApp/ui.R
@@ -1,4 +1,20 @@
 library(shiny)
+library(leaflet)
+library(stringr)
+
+coordinates <- read.csv("LTM_Sample_Coordinates.csv")
+coordinates <- coordinates[complete.cases(coordinates),]
+colnames(coordinates) <- c("lat", "lng", "popup", "ALSC")
+lakedata <- read.csv("LakeData.csv")
+colnames(lakedata)[1] <- "confirmedDate"
+lakedata$ALSC <- str_sub(lakedata$ALSC,2, str_length(lakedata$ALSC))
+lakedata$confirmedDate <- as.Date(lakedata$confirmedDate, "%Y.%m.%d")
+dateValues <- base::unique(lakedata$confirmedDate)
+measurementValues <- colnames(lakedata)[8:ncol(lakedata)]
+coordinates_and_data <- merge(coordinates, lakedata, by = "ALSC")
+library(data.table)
+coord_data_dt <- data.table(coordinates_and_data)
+
 shinyUI(fluidPage(
   titlePanel("Acid Rain in the Adirondacks"),
   sidebarLayout(
@@ -11,6 +27,7 @@ shinyUI(fluidPage(
     ),
     mainPanel(
       "Explore lake chemistry readings around the Adirondacks. The data were collected by the Adirondack Lakes Survey Corporation, and are available at www.adirondacklakesurvey.org.",
+      h6("Andy Hasselwander, 2018"),
       leafletOutput("map1"),
       plotOutput("timeSeries1")
     )
